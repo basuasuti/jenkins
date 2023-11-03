4768609af3ddd16b6a92906c2b4741b67c0f083e
basuasuti <basurajasuti7026@gmail.com>
2023-11-03 16:12:13 +0530

    file uploded

--- sample.java
+++ sample.java
@@ -1,5 +1,6 @@
-class Sample{
-public satatic void main (String[] args){
-System.out.println("hello");
-}
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
 }
